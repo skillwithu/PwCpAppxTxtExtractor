8a744ed565a06c97a097d55d84eaa49b10d738ec
skillwithu <skillwithu@gmail.com>
2025-05-07 20:51:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "8d0fe8b5ae8149455681681253b2ef17"
+api_hash = "21601817"
+bot_token = "7830517412:AAH_l-6iFIw-LhGuUOxY-4AIPuWdKCBjW3Y"
+auth_users = []
